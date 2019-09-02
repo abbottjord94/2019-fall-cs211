eecd60e5995dd879b4965a2792136387ad3e68f0
= <=>
2019-09-02 10:09:12 -0700

    modified:   main.cpp 	deleted:    a.out 	deleted:    firework 	deleted:    firstprogram.cpp 	deleted:    test 	deleted:    test.cpp 	deleted:    testchar.cpp 	modified:   tx 	deleted:    worm 	deleted:    worm.cpp

--- projects/TextEditor/src/main.cpp
+++ projects/TextEditor/src/main.cpp
@@ -1,10 +1,46 @@
 #include "curses.h"
-#include <cstdlib>
 #include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	WINDOW* main_window = NULL;
+	int num_cols = 0, num_rows = 0, row = 0, col = 0;
+	char input = NULL;
+	bool quit = false;
+	main_window = initscr();
+	getmaxyx(main_window, num_rows, num_cols);
+	resize_term(num_rows - 1, num_cols - 1);
+	noecho();
+	keypad(main_window, TRUE);
+	curs_set(TRUE);
 
+	//BEGIN PROGRAM LOGIC:
+	while(!quit) {
+		input = getch();
+		if(input == 7) {
+			if(col > 0) col--;
+			else if(col == 0 && row > 0) {
+				row--;
+			}
+			mvaddch(row,col,' ');
+		}
+		else if(input == 13 || input == 10) {
+			row++;
+			col = 0;
+			refresh();
+		}
+		else if(input != NULL) {
+			mvaddch(row,col,input);
+			col++;
+		}
+		refresh();
+	}
+
+	//END PROGRAM LOGIC
+
+	input = getch();
+	endwin();
 	return 0;
 }
